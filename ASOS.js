ed01947f6c008de834507eccf8579af16c66e517
KhushC-03 <68359616+KhushC-03@users.noreply.github.com>
2020-08-04 14:14:07 +0100

    Add files via upload

---
+++ ASOS.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+const readline = require("readline")
+const { link } = require("fs")
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+rl.question("ASOS Link: ? ", function(link1) {
+var alink = link1
+    ;(async () => {
+        const puppeteer = require('puppeteer')
+        const browser = await puppeteer.launch({headless : false})
+        const [page] = await browser.pages()
+        await page.setViewport({width: 900, height: 1920})
+        await page.goto(alink)
+        const result = await page.evaluate(() => {
+            let headingfromweb = document.querySelectorAll(".colour-size-select")
+            const headinglist = [...headingfromweb]
+            return headinglist.map(h => h.innerText)[1]
+        })
+        console.log(result)
+        await browser.close()
+    })()
+})
+
